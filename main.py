091f2d76f99dc1aada2016419c1d58037335bee5
jatin.ahluwalia.5@gmail.com <jatin.ahluwalia.5@gmail.com>
2025-12-05 00:32:47 +0530

    pydantic

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.output_parsers import CommaSeparatedListOutputParser
+from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 import os
+from pydantic import BaseModel
 from dotenv import load_dotenv
+
+
+class User(BaseModel):
+    name: str
+    age: int
+    address: str
+
 
 load_dotenv()
 
@@ -10,13 +18,13 @@ client = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash", api_key=os.environ["GOOGLE_GENAI_KEY"]
 )
 
-parser = CommaSeparatedListOutputParser()
+parser = PydanticOutputParser(pydantic_object=User)
 
 prompt = ChatPromptTemplate.from_messages(
     messages=[
         {
             "role": "system",
-            "content": "Get all the names from the user input and format them using these instructions: {formatting_instructions}",
+            "content": "Get the data from this format: {formatting_instructions}",
         },
         {"role": "user", "content": "{input}"},
     ]
@@ -27,7 +35,7 @@ chain = prompt | client | parser
 res = chain.invoke(
     {
         "formatting_instructions": parser.get_format_instructions(),
-        "input": "I have banana, apple, orange and grapes",
+        "input": "I am Jatin Ahluwalia, 23 years old male from Delhi, I work in TCS and now I am switching to Bayer and moving to Bangalore",
     }
 )
 
